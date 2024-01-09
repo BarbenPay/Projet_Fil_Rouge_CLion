49fb0881881743a64d30ab07ac3a4597df4e6446
mc2 <mael.cap2vielle@gmail.com>
2024-01-09 21:46:59 +0100

    vv

---
+++ Menu/menu.c
@@ -0,0 +1,44 @@
+//
+// Created by mc2 on 09/01/24.
+//
+#include <stdio.h>
+#include "menu.h"
+
+void viderBuffer() {
+    int c = 0;
+    while (c != '\n' && c != EOF) {
+        c = getchar();
+    }
+}
+
+int choix_langue() {
+    int l;
+    printf("\nSélectioner la langue : \n\nFrançais : 1 \nEnglish : 2 \n\n");
+    scanf("%d", &l);
+    while(l != 1 && l != 2) {
+        printf("\nSélection incorecte, essayer encore : \n\n");
+        viderBuffer();
+        scanf("%d", &l);
+    }
+    return l;
+}
+
+int choix_test(int l) {
+    if (l == 1) {
+        printf("\nSélectioner ce que vous voulez tester : \n\nTraitement d'image : 3 \nTraitement de commande : 4 \n\n");
+    } else {
+        printf("\nSelect what you want to test : \n\nImage processing : 3 \nOrder processing : 4 \n\n");
+    }
+    int t;
+    scanf("%d", &t);
+    while (t != 3 && t != 4) {
+        if (l == 1) {
+            printf("\nSélection incorrecte, essayer encore : \n\n");
+        } else {
+            printf("\nIncorrect selection, try again : \n\n");
+        }
+        viderBuffer();
+        scanf("%d", &t);
+    }
+    return t;
+}
--- main.c
+++ main.c
@@ -4,30 +4,34 @@
 #include "Log/fichier_Log.h"
 #include "Regroupement/Regroupement.h"
 #include "Analyse_Phrase/Analyse.h"
+#include "Menu/menu.h"
 
 int main() {
 
     log_file_init();
-    Phrase* res;
-    res = decoupage("N'avance pas");
-    for (int i = 0; i<res->wordNumber; i++) {
-        printf("%s\n", res->wordArray[i]);
+    int langue = choix_langue();
+    viderBuffer();
+    int test = choix_test(langue);
+
+    if (test == 4) {
+        char phrase[100];
+        printf("\nSaissez votre commande : \n\n");
+        scanf("%s", &phrase);
+        Phrase* res;
+        res = decoupage(phrase);
+        for (int i = 0; i<res->wordNumber; i++) {
+            printf("%s\n", res->wordArray[i]);
+        }
+        wordTypeStruct ** typeWord;
+        typeWord = sentenceToWordTypeArray(res, langue);
+        for(int i = 0; i<res->wordNumber;i++){
+
+            printf("Le mot %s est de type %d\n",typeWord[i]->word, typeWord[i]->typeWord);
+        }
+        analyse(typeWord,res);
+
+        free(res);
+        free(typeWord);
     }
-    wordTypeStruct ** typeWord;
-    typeWord = sentenceToWordTypeArray(res, 1);
-    for(int i = 0; i<res->wordNumber;i++){
-
-        printf("Le mot %s est de type %d\n",typeWord[i]->word, typeWord[i]->typeWord);
-    }
-    analyse(typeWord,res);
-
-    free(res);
-    free(typeWord);
-
-
-
-
-
     return 0;
-
 }
