e7c3122cff885499d94934890b31c0f0820fd4be
mc2 <mael.cap2vielle@gmail.com>
2024-01-09 14:26:19 +0100

    makefile, main et menu

---
+++ cmake-build-debug/CMakeFiles/Projet_Fil_Rouge_CLion.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/mc2/CLionProjects/Projet_Fil_Rouge_CLion/Analyse_Phrase/Analyse.c" "CMakeFiles/Projet_Fil_Rouge_CLion.dir/Analyse_Phrase/Analyse.c.o" "gcc" "CMakeFiles/Projet_Fil_Rouge_CLion.dir/Analyse_Phrase/Analyse.c.o.d"
+  "/home/mc2/CLionProjects/Projet_Fil_Rouge_CLion/Découpage/Decoupage.c" "CMakeFiles/Projet_Fil_Rouge_CLion.dir/Découpage/Decoupage.c.o" "gcc" "CMakeFiles/Projet_Fil_Rouge_CLion.dir/Découpage/Decoupage.c.o.d"
+  "/home/mc2/CLionProjects/Projet_Fil_Rouge_CLion/Log/fichier_Log.c" "CMakeFiles/Projet_Fil_Rouge_CLion.dir/Log/fichier_Log.c.o" "gcc" "CMakeFiles/Projet_Fil_Rouge_CLion.dir/Log/fichier_Log.c.o.d"
+  "/home/mc2/CLionProjects/Projet_Fil_Rouge_CLion/Menu/menu.c" "CMakeFiles/Projet_Fil_Rouge_CLion.dir/Menu/menu.c.o" "gcc" "CMakeFiles/Projet_Fil_Rouge_CLion.dir/Menu/menu.c.o.d"
+  "/home/mc2/CLionProjects/Projet_Fil_Rouge_CLion/Regroupement/Regroupement.c" "CMakeFiles/Projet_Fil_Rouge_CLion.dir/Regroupement/Regroupement.c.o" "gcc" "CMakeFiles/Projet_Fil_Rouge_CLion.dir/Regroupement/Regroupement.c.o.d"
+  "/home/mc2/CLionProjects/Projet_Fil_Rouge_CLion/main.c" "CMakeFiles/Projet_Fil_Rouge_CLion.dir/main.c.o" "gcc" "CMakeFiles/Projet_Fil_Rouge_CLion.dir/main.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
