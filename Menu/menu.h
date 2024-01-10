bb2f406022f2002dca9bc65869841162818b6d40
mc2 <mael.cap2vielle@gmail.com>
2024-01-10 10:07:17 +0100

    MC2

--- Menu/menu.h
+++ Menu/menu.h
@@ -5,8 +5,14 @@
 #ifndef PROJET_FIL_ROUGE_CLION_MENU_H
 #define PROJET_FIL_ROUGE_CLION_MENU_H
 
+#include "../Découpage/Decoupage.h"
+#include "../Log/fichier_Log.h"
+#include "../Regroupement/Regroupement.h"
+#include "../Analyse_Phrase/Analyse.h"
+
 void viderBuffer();
 int choix_langue();
 int choix_test(int);
+void traitement_commande(int);
 
 #endif //PROJET_FIL_ROUGE_CLION_MENU_H
--- main.c
+++ main.c
@@ -1,38 +1,17 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "Découpage/Decoupage.h"
 #include "Log/fichier_Log.h"
-#include "Regroupement/Regroupement.h"
-#include "Analyse_Phrase/Analyse.h"
 #include "Menu/menu.h"
 
-int main() {
-
+int main()
+{
     log_file_init();
     int langue = choix_langue();
     viderBuffer();
     int test = choix_test(langue);
 
-    if (test == 4) {
-        char phrase[100];
-        printf("\nSaissez votre commande : \n\n");
-        viderBuffer();
-        fgets(phrase, 100, stdin);
-        Phrase* res;
-        res = decoupage(phrase);
-        for (int i = 0; i<res->wordNumber; i++) {
-            printf("%s\n", res->wordArray[i]);
-        }
-        wordTypeStruct ** typeWord;
-        typeWord = sentenceToWordTypeArray(res, langue);
-        for(int i = 0; i<res->wordNumber;i++){
-
-            printf("Le mot %s est de type %d\n",typeWord[i]->word, typeWord[i]->typeWord);
-        }
-        analyse(typeWord,res);
-
-        free(res);
-        free(typeWord);
+    if (test == 4)
+    {
+        traitement_commande(langue);
+        printf("Nouvelle");
     }
     return 0;
 }
