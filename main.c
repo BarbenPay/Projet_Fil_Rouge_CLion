a7b2da02d821b5edb763784cf26d24ba57018bff
mc2 <mael.cap2vielle@gmail.com>
2024-01-11 00:55:56 +0100

    MC2

--- main.c
+++ main.c
@@ -4,13 +4,6 @@
 int main()
 {
     log_file_init();
-    Valeur langue = choix_langue();
-    Valeur test = choix_test(langue);
-    if (test.i == 1)
-    {
-    //    traitement_commande(langue);
-        printf("\nNouvelle commande : 1 \nRetour au choix du test : 2 \n\n");
-
-    }
+    choix_langue();
     return 0;
 }
